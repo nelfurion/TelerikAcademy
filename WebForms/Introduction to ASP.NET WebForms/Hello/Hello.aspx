392763b5570169b3f027a389970cfcd6eaec0b33
radostin-angelov <angelovradostin@gmail.com>
2016-01-23 16:28:39 +0200

    introduction to webforms

---
+++ WebForms/Introduction to ASP.NET WebForms/Hello/Hello.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Hello.aspx.cs" Inherits="Hello.Hello" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Hello</title>
+</head>
+<body>
+    <ul ID="eventsList" runat="server" />
+    <form id="form_hello" runat="server">
+        <div>
+            <asp:label ID="greetLabel" runat="server">Type your name in the box</asp:label>
+            <asp:TextBox ID="nameTextBox" runat="server" />
+            <asp:Button OnClick="Greet" runat="server" ID="sumButton" Text="Submit"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForms/Introduction to ASP.NET WebForms/Hello/Hello.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace Hello
+{
+    public partial class Hello : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HtmlGenericControl newLi = new HtmlGenericControl("li");
+            newLi.InnerText = "Page_Load()";
+            this.eventsList.Controls.Add(newLi);
+        }
+
+        protected override void OnInitComplete(EventArgs e)
+        {
+            HtmlGenericControl newLi = new HtmlGenericControl("li");
+            newLi.InnerText = "OnInitComplete()";
+            this.eventsList.Controls.Add(newLi);
+            base.OnInitComplete(e);
+        }
+
+        protected override void OnPreRenderComplete(EventArgs e)
+        {
+            HtmlGenericControl newLi = new HtmlGenericControl("li");
+            newLi.InnerText = "OnPreRenderComplete()";
+            this.eventsList.Controls.Add(newLi);
+            base.OnPreRenderComplete(e);
+        }
+
+        protected override void OnLoadComplete(EventArgs e)
+        {
+            HtmlGenericControl newLi = new HtmlGenericControl("li");
+            newLi.InnerText = "OnLoadComplete()";
+            this.eventsList.Controls.Add(newLi);
+            base.OnLoadComplete(e);
+        }
+
+        protected override void OnPreInit(EventArgs e)
+        {
+            HtmlGenericControl newLi = new HtmlGenericControl("li");
+            newLi.InnerText = "OnPreInit()";
+            this.eventsList.Controls.Add(newLi);
+            base.OnPreInit(e);
+        }
+
+        protected override void OnPreLoad(EventArgs e)
+        {
+            HtmlGenericControl newLi = new HtmlGenericControl("li");
+            newLi.InnerText = "OnPreLoad()";
+            this.eventsList.Controls.Add(newLi);
+            base.OnPreLoad(e);
+        }
+
+        protected override void OnError(EventArgs e)
+        {
+            HtmlGenericControl newLi = new HtmlGenericControl("li");
+            newLi.InnerText = "OnError()";
+            this.eventsList.Controls.Add(newLi);
+            base.OnError(e);
+        }
+
+        protected override void OnSaveStateComplete(EventArgs e)
+        {
+            HtmlGenericControl newLi = new HtmlGenericControl("li");
+            newLi.InnerText = "OnSaveStateComplete()";
+            this.eventsList.Controls.Add(newLi);
+            base.OnSaveStateComplete(e);
+        }
+
+        protected void Greet(object sender, EventArgs e)
+        {
+            this.greetLabel.Text = "Hello " + this.nameTextBox.Text;
+        }
+    }
+}
